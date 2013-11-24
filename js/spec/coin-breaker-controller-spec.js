6b8368dbdcd38e75667e4d181891296b93cbb7eb
pgchamberlin <pgchamberlin@googlemail.com>
2013-11-24 15:57:29 +0000

    Updated controller and test spec to work with coin breaker util

--- js/spec/coin-breaker-controller-spec.js
+++ js/spec/coin-breaker-controller-spec.js
@@ -3,6 +3,23 @@ define([
 ], function(CoinBreakerController) {
 
     var controller = CoinBreakerController;
+        
+    /**
+     * We're going to implement the data provider pattern, which Jasmine
+     * doesn't have built in.
+     *
+     * I have lifted this method from: 
+     * http://blog.jphpsf.com/2012/08/30/drying-up-your-javascript-jasmine-tests
+     */
+    using = function(name, values, func) {
+        for (var i = 0, count = values.length; i < count; i++) {
+            if (Object.prototype.toString.call(values[i]) !== '[object Array]') {
+                values[i] = [values[i]];
+            }
+            func.apply(this, values[i]);
+            jasmine.currentEnv_.currentSpec.description += ' (with "' + name + '" using ' + values[i].join(', ') + ')';
+        }
+    };
 
     describe('CoinBreakerController', function() {
 
@@ -19,21 +36,6 @@ define([
 
             // check the view subscribes to the mock view with the onViewChange event
             expect(mockView.subscribe).toHaveBeenCalledWith(controller.onViewChange);
-        });
-
-        it('should update the model when the view updates', function() {
-            
-            var mockModel = {
-                setData: function(data) {}
-            },
-            mockData = 'thisIsMockData';
-            spyOn(mockModel, 'setData');
-
-            controller.setModel(mockModel);
-
-            controller.onViewChange(mockData);
-
-            expect(mockModel.setData).toHaveBeenCalledWith(mockData);
         });
     });
 });
--- js/src/controllers/coin-breaker-controller.js
+++ js/src/controllers/coin-breaker-controller.js
@@ -1,20 +1,60 @@
-define([], function() {
+define([
+    '../util/coin-breaker'
+], function(CoinBreaker) {
 
-    var observe = function(subject) {
+    var 
+        /**
+         * MVC controller behaviour methods
+         */    
+        observe = function(subject) {
             subject.subscribe(onViewChange);
         },
-        onViewChange = function(amount) {
-            model.setData(amount);
+        onViewChange = function(input) {
+            handleViewChange(input);
         },
         model,
         setModel = function(m) {
             model = m;
+        },
+        /**
+         *  
+         */
+
+        /**
+         * This handler takes the updated input value from
+         * the view, gets the new value for the model's
+         * data, and then sets it back in the model.
+         */
+        handleViewChange = function(input) {
+            var newData = getNewData(input);
+            model.setData(newData);
+        },
+
+        /**
+         * Takes an amount in pence as input, uses the coin
+         * breaker utility to work out the minimum Sterling
+         * coin composition, and templates these values into
+         * a suitable model data object
+         */
+        getNewData = function(input) {
+            var optimalChange = CoinBreaker.denominate(input);
+            return {
+                'totalAmount': input,
+                'ones': optimalChange['ones'],
+                'twos': optimalChange['twos'],
+                'fives': optimalChange['fives'],
+                'tens': optimalChange['tens'],
+                'twenties': optimalChange['twenties'],
+                'fifties': optimalChange['fifties'],
+                'pounds': optimalChange['pounds'],
+                'twoPounds': optimalChange['twoPounds']
+            }
         };
     
     return {
         observe: observe,
         setModel: setModel,
-        //
+        // exposed for testing purposes:
         onViewChange: onViewChange
     }
 });
