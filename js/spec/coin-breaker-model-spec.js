28210d5663a9a8508a1838b95ce698213fa73653
pgchamberlin <pgchamberlin@googlemail.com>
2013-11-24 14:27:47 +0000

    Added subscriber support to model with tests

--- js/spec/coin-breaker-model-spec.js
+++ js/spec/coin-breaker-model-spec.js
@@ -1,6 +1,31 @@
 define([
     'js/src/models/coin-breaker-model'
 ], function(CoinBreakerModel) {
-    // no tests yet
+
+    var model;
+
+    describe('CoinBreakerModel', function() {
+
+        beforeEach(function() {
+            model = CoinBreakerModel;
+        });
+
+        it('should accept subscribers and notify them onChange', function() {
+
+            var mockSubscriber = {
+                callback: function() {}
+            },
+            mockUpdateValue = 'amockvalue';
+            spyOn(mockSubscriber, 'callback');
+
+            // subscribe the mock
+            model.subscribe(mockSubscriber.callback);
+
+            model.notify(mockUpdateValue);
+
+            // check that the mock was called with the mock value
+            expect(mockSubscriber.callback).toHaveBeenCalledWith(mockUpdateValue);
+        });
+    });
 });
 
--- js/src/models/coin-breaker-model.js
+++ js/src/models/coin-breaker-model.js
@@ -1,4 +1,19 @@
 define([], function() {
-    
-    return {}
+
+    var subscribers = [],
+        subscribe = function(callback) {
+            subscribers.push(callback);
+        },
+        notify = function(notification) {
+            for (i in subscribers) {
+                if (subscribers.hasOwnProperty(i)) {
+                    subscribers[i](notification);
+                }
+            }
+        };
+
+    return {
+        subscribe: subscribe,
+        notify: notify
+    }
 });
